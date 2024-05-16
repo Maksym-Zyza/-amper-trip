4fdcd12a686356421da202d9539ae81bfcbfe3ba
Maksym-Zyza <mmziza88@gmail.com>
2024-05-16 23:30:11 +0300

    added icons

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -4,5 +4,58 @@ import { ReactComponent as Catalog } from './Catalog.svg';
 import { ReactComponent as Favorites } from './Favorites.svg';
 import { ReactComponent as BurgerBtn } from './BurgerBtn.svg';
 import { ReactComponent as Camper } from './Camper.svg';
+import { ReactComponent as Camper_mid } from './Camper_mid.svg';
+import { ReactComponent as Camper_fully } from './Camper_fully.svg';
+import { ReactComponent as Automatic } from './Automatic.svg';
+import { ReactComponent as Bed } from './Bed.svg';
+import { ReactComponent as Calendar } from './Calendar.svg';
+import { ReactComponent as CD } from './CD.svg';
+import { ReactComponent as Freezer } from './Freezer.svg';
+import { ReactComponent as Gas } from './Gas.svg';
+import { ReactComponent as Kitchen } from './Kitchen.svg';
+import { ReactComponent as Like } from './Like.svg';
+import { ReactComponent as Location } from './Location.svg';
+import { ReactComponent as Microwave } from './Microwave.svg';
+import { ReactComponent as Paper } from './Paper.svg';
+import { ReactComponent as Petrol } from './Petrol.svg';
+import { ReactComponent as Plate } from './Plate.svg';
+import { ReactComponent as Radio } from './Radio.svg';
+import { ReactComponent as Route } from './Route.svg';
+import { ReactComponent as Screen } from './Screen.svg';
+import { ReactComponent as Search } from './Search.svg';
+import { ReactComponent as Shower } from './Shower.svg';
+import { ReactComponent as Users } from './Users.svg';
+import { ReactComponent as Water } from './Water.svg';
+import { ReactComponent as Wind } from './Wind.svg';
 
-export { Logo, Home, Catalog, Favorites, BurgerBtn, Camper };
+export {
+  Logo,
+  Home,
+  Catalog,
+  Favorites,
+  BurgerBtn,
+  Camper,
+  Camper_mid,
+  Camper_fully,
+  Automatic,
+  Bed,
+  Calendar,
+  CD,
+  Freezer,
+  Gas,
+  Kitchen,
+  Like,
+  Location,
+  Microwave,
+  Paper,
+  Petrol,
+  Plate,
+  Radio,
+  Route,
+  Screen,
+  Search,
+  Shower,
+  Users,
+  Water,
+  Wind,
+};
