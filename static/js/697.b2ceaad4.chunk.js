0c42ea0c5773f008cb0558bb619e68ecead2230d
Maksym-Zyza <66421083+Maksym-Zyza@users.noreply.github.com>
2024-05-19 18:16:59 +0000

    Deploying to gh-pages from @ Maksym-Zyza/camper-trip@e3cdb839a47dc46adf0d1e066167022d7796d8c3 🚀

--- static/js/697.b2ceaad4.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[697],{5697:function(e,t,r){r.r(t);r(2791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"FavoritesPage"})}}}]);
-//# sourceMappingURL=697.b2ceaad4.chunk.js.map
