158a5fd44b9d70295132fb4aa16cf45089b5e104
Maksym-Zyza <mmziza88@gmail.com>
2024-05-20 23:49:03 +0300

    added form patterns

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -21,12 +21,27 @@ const BookingForm = () => {
         Stay connected! We are always ready to help you.
       </span>
       <form className={styles.form} onSubmit={handleSubmit}>
-        <input type="text" name="name" placeholder="Name" />
-        <input type="email" name="email" placeholder="Email" />
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+        />
         <div className={styles.dateBar}>
-          <input type="date" name="date" />
+          <input type="date" name="date" min="2020-01-01" max="2025-12-31" />
         </div>
-        <textarea name="comment" placeholder="Comment" rows="4"></textarea>
+        <textarea
+          name="comment"
+          placeholder="Comment"
+          rows="4"
+          pattern="^[a-zA-Z0-9\s.,!?']*$"
+        ></textarea>
         <Button text="Send" type="submit" />
       </form>
     </div>
